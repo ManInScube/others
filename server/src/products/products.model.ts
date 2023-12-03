103fa29d8500a4208bee8e8aa7398492fb7f9f1e
unknown <kamilgwork@gmail.com>
2023-12-03 11:53:48 +0300

    added products component

---
+++ server/src/products/products.model.ts
@@ -0,0 +1,31 @@
+import {Model, Table, Column } from "sequelize-typescript";
+
+
+@Table
+export class Product extends Model {
+    @Column
+    name: string;
+
+    @Column
+    manufacturer: string;
+
+    @Column
+    size: string; //enum?
+
+    @Column({defaultValue: 0})
+    price: number;
+
+    @Column
+    description: string;
+
+    @Column
+    color: string;
+
+    @Column
+    images: string;
+
+    @Column({defaultValue: 0})
+    in_stock: number;
+
+    //vendor and sale?
+}
